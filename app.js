8baefea4491bf4a50ac1880076633b78020c2150
dslyvka <daniilslyvka@gmail.com>
2021-09-25 19:20:17 +0300

    CreateMarkupFunc

--- app.js
+++ app.js
@@ -63,3 +63,28 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryListRef = document.querySelector('.js-gallery');
+
+const createGalleryMarkup = images => {
+  return images.map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  ).join('');
+};
+
+const galleryItemsMarkup = createGalleryMarkup(galleryItems);
+galleryListRef.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
